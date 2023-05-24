12e514ea8b7f97195daf1d96244e2a51bbadf92d
Abel <abelzeleke5173@gmail.com>
2023-05-24 09:44:34 +0300

    syncfusion

--- front-end/src/app/side-bar/side-bar.component.ts
+++ front-end/src/app/side-bar/side-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild} from '@angular/core';
-import { SidebarComponent } from '@syncfusion/ej2-angular-navigations';
+import { SidebarComponent, TreeViewComponent } from '@syncfusion/ej2-angular-navigations';
 
 @Component({
     selector: 'app-side-bar',
@@ -8,34 +8,80 @@ import { SidebarComponent } from '@syncfusion/ej2-angular-navigations';
 })
 export class SideBarComponent {
   @ViewChild('sidebarTreeviewInstance')
-  public sidebarTreeviewInstance!: SidebarComponent;
-  public data: { [key: string]: Object }[] = [
-    {
-        nodeId: '01', nodeText: 'Installation',
-    },
-    {
-        nodeId: '02', nodeText: 'Deployment',
-    },
-    {
-        nodeId: '03', nodeText: 'Quick Start',
-    },
-    {
-        nodeId: '04', nodeText: 'Components',
-        nodeChild: [
-            { nodeId: '04-01', nodeText: 'Calendar' },
-            { nodeId: '04-02', nodeText: 'DatePicker' },
-            { nodeId: '04-03', nodeText: 'DateTimePicker' },
-            { nodeId: '04-04', nodeText: 'DateRangePicker' },
-            { nodeId: '04-05', nodeText: 'TimePicker' },
-            { nodeId: '04-06', nodeText: 'SideBar' }
-        ]
+    public sidebarTreeviewInstance!: SidebarComponent;
+    @ViewChild('treeviewInstance')
+    public treeviewInstance!: TreeViewComponent;
+    public width: string = '290px';
+    public enableDock: boolean = true;
+    public dockSize:string ="44px";
+    public mediaQuery: string = ('(min-width: 600px)');
+    public target: string = '.main-content';
+  
+    public data: Object[] = [
+        {
+            nodeId: '01', nodeText: 'Installation', iconCss: 'icon-microchip icon',
+        },
+        {
+            nodeId: '02', nodeText: 'Deployment', iconCss: 'icon-thumbs-up-alt icon',
+        },
+        {
+            nodeId: '03', nodeText: 'Quick Start', iconCss: 'icon-docs icon',
+        },
+        {
+            nodeId: '04', nodeText: 'Components', iconCss: 'icon-th icon',
+            nodeChild: [
+                { nodeId: '04-01', nodeText: 'Calendar', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '04-02', nodeText: 'DatePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '04-03', nodeText: 'DateTimePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '04-04', nodeText: 'DateRangePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '04-05', nodeText: 'TimePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '04-06', nodeText: 'SideBar', iconCss: 'icon-circle-thin icon' }
+            ]
+        },
+        {
+            nodeId: '05', nodeText: 'API Reference', iconCss: 'icon-code icon',
+            nodeChild: [
+                { nodeId: '05-01', nodeText: 'Calendar', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '05-02', nodeText: 'DatePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '05-03', nodeText: 'DateTimePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '05-04', nodeText: 'DateRangePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '05-05', nodeText: 'TimePicker', iconCss: 'icon-circle-thin icon' },
+                { nodeId: '05-06', nodeText: 'SideBar', iconCss: 'icon-circle-thin icon' }
+            ]
+        },
+        {
+            nodeId: '06', nodeText: 'Browser Compatibility', iconCss: 'icon-chrome icon'
+        },
+        {
+            nodeId: '07', nodeText: 'Upgrade Packages', iconCss: 'icon-up-hand icon'
+        },
+        {
+            nodeId: '08', nodeText: 'Release Notes', iconCss: 'icon-bookmark-empty icon'
+        },
+        {
+            nodeId: '09', nodeText: 'FAQ', iconCss: 'icon-help-circled icon'
+        },
+        {
+            nodeId: '10', nodeText: 'License', iconCss: 'icon-doc-text icon'
+        }
+    ];
+    public field:Object ={ dataSource: this.data, id: 'nodeId', text: 'nodeText', child: 'nodeChild', iconCss: 'iconCss' };
+
+    public onCreated(args: any) {
+         this.sidebarTreeviewInstance.element.style.visibility = '';
     }
-  ];
-  public width: string = '220px';
-  public target: string = '.main-sidebar-content';
-  public mediaQuery: string = '(min-width: 600px)';
-  public fields: object = { dataSource: this.data, id: 'nodeId', text: 'nodeText', child: 'nodeChild' };
-  toggle() {
-    this.sidebarTreeviewInstance.toggle();
-  };
+    public onClose(args: any) {
+        this.treeviewInstance.collapseAll();
+    }
+    openClick() {
+        if(this.sidebarTreeviewInstance.isOpen)
+        {
+            this.sidebarTreeviewInstance.hide();
+            this.treeviewInstance.collapseAll();
+        }
+        else {
+            this.sidebarTreeviewInstance.show();
+            this.treeviewInstance.expandAll();
+        }  
+    }
 }
