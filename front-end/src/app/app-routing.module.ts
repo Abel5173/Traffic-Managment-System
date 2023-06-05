a7fd8f8aa3e0b616e96d87cefa0e95e236994022
Bereket <berekettadelemulu210@gmail.com>
2023-06-05 15:00:40 +0300

    routing added

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { PenalityGridComponent } from './penality-grid/penality-grid.component';
 import { PenaltyFormComponent } from './penalty-form/penalty-form.component';
 import { AccidentTableComponent } from './accident-table/accident-table.component';
 import { DrivertableComponent } from './drivertable/drivertable.component';
+import { OfficertableComponent } from './officertable/officertable.component';
+import { VehicletableComponent } from './vehicletable/vehicletable.component';
 
 const routes: Routes = [
   { path: '',
-  component: AccidentFormComponent
+  component: DashboardComponent
   
 },
 {
-  path : 'penality' , component : PenalityGridComponent
+  path : 'penalitytable' , component : PenalityGridComponent
 },
 {
   path : 'penalityform' , component : PenaltyFormComponent
@@ -23,7 +25,16 @@ const routes: Routes = [
   path : 'accidenttable' , component : AccidentTableComponent
 },
 {
+  path : 'accidentform' , component : AccidentFormComponent
+},
+{
   path : 'drivertable' , component : DrivertableComponent
+},
+{
+  path : 'officertable' , component : OfficertableComponent
+},
+{
+  path : 'vehicletable' , component : VehicletableComponent
 }
 ];
 
